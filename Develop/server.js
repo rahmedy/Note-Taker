fdc563abe097bc3bc97730628e41a25f4bda0ebd
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-08-28 16:51:29 -0400

    WIP

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,9 @@ let apidb = require("./db/db.json")
 const util = require("util");
 
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log("listeing on port: " + PORT);
+})
+
