7b9377f39ef5c987d4b8bfb4d87cf8b9bf2ab3cb
Rayan Ahmedy <rayanahmedy@Rayans-MBP.attlocal.net>
2020-08-31 22:55:31 -0400

    updated GET api

--- Develop/server.js
+++ Develop/server.js
@@ -7,7 +7,55 @@ const util = require("util");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+
+
+
+app.use(express.static("public"));
+
+  app.get("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", function(err, data) {
+        if (err) throw err;
+ res.json (JSON.parse(data));
+        console.log(JSON.parse(data));
+    });
+
+
+  });
+
+  app.post("/api/notes" , function(req,res) {
+
+    fs.readFile("db/db.json", function(err, data){
+        if (err) throw err;
+        res.json (JSON.parse(data));
+        console.log(
+    });
+  });
+
+
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+
+
+
+
+
 app.listen(PORT, () => {
-    console.log("listeing on port: " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 })
 
