4d58b4cc008e826877b69434c6094d2c6d5e3921
Rayan Ahmedy <rayanahmedy@Rayans-MBP.attlocal.net>
2020-09-02 17:40:38 -0400

    setup path

--- Develop/server.js
+++ Develop/server.js
@@ -1,61 +1,48 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-let apidb = require("./db/db.json")
-const util = require("util");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let apidb = require('./db/db.json');
+const util = require('util');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+//  This will be the get and handle the get requests 
 
-
-app.use(express.static("public"));
-
-  app.get("/api/notes", function(req, res) {
-    fs.readFile("db/db.json", function(err, data) {
-        if (err) throw err;
- res.json (JSON.parse(data));
-        console.log(JSON.parse(data));
-    });
-
-
+app.get('/api/notes', function (req, res) {
+  fs.readFile('db/db.json', function (err, data) {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+    console.log(JSON.parse(data));
   });
+});
 
-  app.post("/api/notes" , function(req,res) {
+// This will allow for new nets and handle the request 
 
-    fs.readFile("db/db.json", function(err, data){
-        if (err) throw err;
-        res.json (JSON.parse(data));
-        // console.log()
-    });
+app.post('/api/notes', function (req, res) {
+  fs.readFile('db/db.json', function (err, data) {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+    // console.log()
   });
+});
 
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
-
-
-
-
-
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log("Server listening on: http://localhost:" + PORT);
-})
-
+  console.log('Server listening on: http://localhost:' + PORT);
+});
