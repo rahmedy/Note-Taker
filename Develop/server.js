e1d02a009187c2d50dac74de1fcfcee353420458
Rayan Ahmedy <rayanahmedy@Rayans-MBP.attlocal.net>
2020-09-02 23:41:13 -0400

    wip on the routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-let apidb = require('./db/db.json');
+let api = require('./db/db.json');
 const util = require('util');
 
 const app = express();
@@ -17,29 +17,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //  This will be the get and handle the get requests 
 
-app.get('/api/notes', function (req, res) {
-  fs.readFile('db/db.json', function (err, data) {
-    if (err) throw err;
-    res.json(JSON.parse(data));
-    console.log(JSON.parse(data));
-  });
+app.get('/api/notes', (req, res) => {
+  res.json(api);
+  // res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")));
+
+  // console.log(JSON.parse(data));
 });
+
 
 // This will allow for new nets and handle the request 
 
-app.post('/api/notes', function (req, res) {
-  fs.readFile('db/db.json', function (err, data) {
+app.post('/api/notes', (req, res) => {
+ 
+  const newN = req.body;
+  let id = Math.floor(Math.random() * 10000);
+  newN.id = id;
+  api.push(newN);
+
+  fs.writeFile("db/db.json", JSON.stringify(api)),(err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("Note written!");
+    }
+  };
+  res.json(newN);
+  // fs.readFile('db/db.json', function (err, data) {
+  // if (err) throw err;
+  // res.json(JSON.parse(data));
+  // console.log()
+});
+
+
+app.delete("/api/notes/:id", (req, res) => {
+  const noteID = req.params.id;
+  const newNotes = api.filter((note) => note.id !== Number(noteID));
+  fs.writeFile("db/db.json", JSON.stringify(newNotes), (err) => {
     if (err) throw err;
-    res.json(JSON.parse(data));
-    // console.log()
+    res.json(newNotes);
+    location.reload();
   });
 });
 
-app.get('/notes', function (req, res) {
+
+
+
+
+app.get('/notes', (req, res) =>{
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', function (req, res) {
+// Should return the index.html file if nothing is specified 
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
